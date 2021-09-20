e5c9a8441dd51f2a082e940cc9fd41d36be1e67d
jjthug <jossychemmazham@gmail.com>
2021-09-20 09:55:24 +0530

    immutable issue with _cap during creation time

--- contracts/token.sol
+++ contracts/token.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 contract MyToken is ERC20Capped {
 
 constructor() ERC20("JJToken", "JJT") ERC20Capped(10000){
-    _mint(msg.sender,1000);
+    //_mint(msg.sender,1000);
+    ERC20._mint(msg.sender, 1000);
 }
 }
